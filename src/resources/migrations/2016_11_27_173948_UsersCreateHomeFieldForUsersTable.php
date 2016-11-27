5cc6009ecfd2ac4ec7bd0102078ea13ddaa2c645
Egemen Berker Kızılcan <egemen@kenarkose.com>
2016-11-27 23:35:03 +0300

    Renamed migration

--- src/resources/migrations/2016_11_27_173948_UsersCreateHomeFieldForUsersTable.php
+++ src/resources/migrations/2016_11_27_173948_UsersCreateHomeFieldForUsersTable.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHomeFieldForUsersTable extends Migration
+class UsersCreateHomeFieldForUsersTable extends Migration
 {
     /**
      * Run the migrations.
